105e97993310df2b5145f4415aadc57b0fff3206
Bradley Horlander <78397249+BradleySH@users.noreply.github.com>
2022-01-22 22:33:39 -0500

    modules and use

---
+++ restaurant/target/rls/debug/deps/restaurant-011a30607782ff38.d
@@ -0,0 +1,5 @@
+/Users/bradleyhorlander/rust_training/restaurant/target/rls/debug/deps/restaurant-011a30607782ff38.rmeta: src/lib.rs
+
+/Users/bradleyhorlander/rust_training/restaurant/target/rls/debug/deps/restaurant-011a30607782ff38.d: src/lib.rs
+
+src/lib.rs:
---
+++ restaurant/target/rls/debug/deps/restaurant-c5d078d6c0e5d31a.d
@@ -0,0 +1,5 @@
+/Users/bradleyhorlander/rust_training/restaurant/target/rls/debug/deps/restaurant-c5d078d6c0e5d31a.rmeta: src/lib.rs
+
+/Users/bradleyhorlander/rust_training/restaurant/target/rls/debug/deps/restaurant-c5d078d6c0e5d31a.d: src/lib.rs
+
+src/lib.rs:
